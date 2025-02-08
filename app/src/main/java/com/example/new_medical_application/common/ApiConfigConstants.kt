02fd665c533ce6d09e33602b380369846babd0a5
mihneaburlacu <102325337+mihneaburlacu@users.noreply.github.com>
2025-02-08 16:34:31 +0200

    fix: text input box stroke on focus

--- app/src/main/java/com/example/new_medical_application/common/ApiConfigConstants.kt
+++ app/src/main/java/com/example/new_medical_application/common/ApiConfigConstants.kt
@@ -3,9 +3,9 @@ package com.example.new_medical_application.common
 object ApiConfigConstants {
     const val FITBIT_BASE_URL = "https://api.fitbit.com"
     const val FITBIT_REFRESH_TOKEN =
-        "06e7674f85f3439701d492ab8110b9d87272e718aba3b012aa53ce50d6e6018d"
+        "9aebd76bf1b27d46b934f5f5d550686c7d58ca61fb4a77a02a66e6c32eb0663c"
     const val ACCESS_TOKEN =
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1JHR1giLCJzdWIiOiJCU1lUVEsiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJwcm8gcm51dCByc2xlIHJjZiByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNzM4NDE1NTQzLCJpYXQiOjE3MzgyNDI3NDN9.3RkBbpmR6m_brB-rGw3Pcym-2VLsIxdakxqAEmMURO0"
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1JHR1giLCJzdWIiOiJCU1lUVEsiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJwcm8gcm51dCByc2xlIHJjZiByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNzM5MTkzMTM3LCJpYXQiOjE3MzkwMjAzMzd9.J0h4OP-4TA5wYFwMeV__IqqVXGdXtjVfYWY0-s8MDkU"
 
     const val HEALTH_BASE_URL = "https://health.gov/"
 }
